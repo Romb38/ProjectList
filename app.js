6300aea0edf27f61e6cca0fec69b796cc8b41452
romb38 <r.barbier38@gmail.com>
2024-09-19 00:44:40 +0200

    Testing other things

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { eld } = await import('./node_modules/eld/src/languageDetector')
+const { eld } = await import('https://cdn.skypack.dev/eld');
 
 function getLanguageNameInNativeForm(isoCode) {
     if (typeof isoCode !== 'string' || isoCode.length !== 2) {
